124772c030ad4f654a6ab1e4cb0a7195091254c2
Louai Al-Khanji <1539580+al-khanji@users.noreply.github.com>
2021-06-20 07:46:43 +0100

    Simplify initial package setup

--- init.el
+++ init.el
@@ -69,20 +69,18 @@
 ;; basic package setup
 (progn
   (require 'package)
-  (dolist (e '(("gnu"   . "https://elpa.gnu.org/packages/")
-               ("melpa" . "https://melpa.org/packages/")
-               ("org"   . "https://orgmode.org/elpa/")))
-    (add-to-list 'package-archives e))
-  (setq package-user-dir (expand-file-name "packages" user-emacs-directory))
+  (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                           ("org" . "https://orgmode.org/elpa/")
+                           ("elpa" . "https://elpa.gnu.org/packages/")))
   (package-initialize)
 
+  (unless package-archive-contents
+    (package-refresh-contents))
+
   (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
     (package-install 'use-package))
 
-  (eval-when-compile
-    (require 'use-package))
-
+  (require 'use-package)
   (setq use-package-always-ensure t))
 
 ;; Custom github bits and pieces
