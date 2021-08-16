df8a300d5618e5eb2a2511c41420b5c00326ca9b
Louai Al-Khanji <1539580+al-khanji@users.noreply.github.com>
2021-08-16 14:15:37 +0100

    Set up package auto updates

--- init.el
+++ init.el
@@ -72,6 +72,15 @@
   (require 'use-package)
   (setq use-package-always-ensure t))
 
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
+
 ;; Custom github bits and pieces
 (progn
   (defvar *local-github-dir* (expand-file-name "~/github"))
