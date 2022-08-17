955a13188b843275b4a050793523e726f3ec3c12
Louai Al-Khanji <1539580+al-khanji@users.noreply.github.com>
2022-08-17 12:26:26 +0300

    Fix install script

--- install_macos.sh
+++ install_macos.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -eux
 
-if [ ! which brew ]
-do
+if ! which brew
+then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+brew tap homebrew/cask-fonts
+brew install --cask font-iosevka
+
 brew tap d12frosted/emacs-plus
-brew install font-iosevka
 brew install emacs-plus --with-native-comp --with-imagemagick --with-xwidgets
-ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications
+
+ln -sf /usr/local/opt/emacs-plus/Emacs.app /Applications
