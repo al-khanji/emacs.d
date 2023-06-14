2e858e2331249da1b1f6255f23f0361356dc74a2
Louai Al-Khanji <louai@astranis.com>
2023-06-14 12:13:27 -0700

    Improve how Iosevka Term version number is represented

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -5,12 +5,13 @@ sudo apt install -y libtool libtool-bin cmake
 
 sudo snap install emacs --classic
 
-TTF_IOSEVKA_TERM_ZIP="ttf-iosevka-term-24.1.3.zip"
+TTF_IOSEVKA_VERSION="24.1.3"
+TTF_IOSEVKA_TERM_ZIP="ttf-iosevka-term-$TTF_IOSEVKA_VERSION.zip"
 DOWNLOADS_DIR="$HOME/Downloads"
 FONTS_DIR="$HOME/.local/share/fonts"
 install -d "$DOWNLOADS_DIR"
 cd "$DOWNLOADS_DIR"
-curl -sLO "https://github.com/be5invis/Iosevka/releases/download/v24.1.3/$TTF_IOSEVKA_TERM_ZIP"
+curl -sLO "https://github.com/be5invis/Iosevka/releases/download/v$TTF_IOSEVKA_VERSION/$TTF_IOSEVKA_TERM_ZIP"
 cd -
 install -d "$FONTS_DIR"
 cd "$FONTS_DIR"
