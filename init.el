cd6ef3829fcd3fdc93f6be515517ec08b4003905
Louai Al-Khanji <louai@astranis.com>
2023-06-14 14:13:17 -0700

    Automatically keep elpa keys up to date

--- init.el
+++ init.el
@@ -23,6 +23,8 @@
   (require 'use-package)
   (setq use-package-always-ensure t))
 
+(use-package gnu-elpa-keyring-update)
+
 (use-package no-littering
   :config
   (setq auto-save-file-name-transforms `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))
