e928b99b5debc854fa7aaee9d5569980dd3dee38
Louai Al-Khanji <louai@astranis.com>
2023-06-14 10:39:40 -0700

    Remove config that just sets the defaults

--- init.el
+++ init.el
@@ -494,17 +494,6 @@ Prepends by default, append by setting APPEND to non-nil."
   (with-eval-after-load 'winum
     (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
   :config
-  (treemacs-follow-mode t)
-  (treemacs-filewatch-mode t)
-  (treemacs-fringe-indicator-mode 'always)
-  (pcase (cons (not (null (executable-find "git")))
-               (not (null treemacs-python-executable)))
-    (`(t . t)
-     (treemacs-git-mode 'deferred))
-    (`(t . _)
-     (treemacs-git-mode 'simple)))
-
-  (treemacs-hide-gitignored-files-mode nil)
   :bind (:map global-map
               ("M-0"       . treemacs-select-window)
               ("C-x t 1"   . treemacs-delete-other-windows)
