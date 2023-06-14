90909738d2b7e0a329eca861d3ad492d55d0b9c9
Louai Al-Khanji <louai@astranis.com>
2023-06-14 10:46:59 -0700

    Add clang-format package

--- init.el
+++ init.el
@@ -448,6 +448,8 @@ Prepends by default, append by setting APPEND to non-nil."
   :hook ((c-mode c++-mode objc-mode cuda-mode) .
          (lambda () (require 'ccls) (lsp-deferred))))
 
+(use-package clang-format)
+
 (use-package lsp-ui
   :after (lsp-mode)
   :commands lsp-ui-mode
