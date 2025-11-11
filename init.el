c0b3367f47c21d8ff27b4e5e406c9abc68ddc48c
Louai Al-Khanji <louai.khanji@gmail.com>
2025-11-11 08:55:05 -0800

    Add claude config

--- init.el
+++ init.el
@@ -479,6 +479,21 @@
 (use-package ultra-scroll
   :init (ultra-scroll-mode))
 
+;; install claude-code.el
+(use-package claude-code
+  :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
+  :custom (claude-code-terminal-backend 'vterm)
+  :config
+  ;; optional IDE integration with Monet
+  ;; (add-hook 'claude-code-process-environment-functions #'monet-start-server-function)
+  ;; (monet-mode 1)
+  (claude-code-mode)
+  :bind-keymap ("C-c c" . claude-code-command-map)
+
+  ;; Optionally define a repeat map so that "M" will cycle thru Claude auto-accept/plan/confirm modes after invoking claude-code-cycle-mode / C-c M.
+  :bind
+  (:repeat-map my-claude-code-map ("M" . claude-code-cycle-mode)))
+
 (server-start)
 
 (load (expand-file-name "site-config.el" "~/.emacs.d") 'noerror)
